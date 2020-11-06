1b67e3390d6f83db226952ebba2bd61eda36ae46
kNgo8249 <kenguin8249@gmail.com>
2020-11-05 19:01:55 -0600

    Created features component

---
+++ frontend/src/components/LandingPage/Features/Features.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import {
+    Card, CardTitle, CardText, CardDeck,
+    CardBody
+  } from 'reactstrap';
+  
+function Features() {
+    return (
+        
+        <CardDeck>
+            <Card>
+                <CardBody>
+                    <CardTitle tag="h4">Guess Prices</CardTitle>
+                    <CardText tag="h6">Get predictions on future stock prices so you can make more informed buying decisions.</CardText>
+                </CardBody>
+            </Card>
+            <Card>
+                <CardBody>
+                    <CardTitle tag="h4">Get Stock Information</CardTitle>
+                    <CardText tag="h6">Get the latest information on stocks, including prices and changes.</CardText>
+                </CardBody>
+            </Card>
+            <Card>
+                <CardBody>
+                    <CardTitle tag="h4">Get Current News</CardTitle>
+                    <CardText tag="h6">Get the current news on things like the weather and company updates.</CardText>
+                </CardBody>
+            </Card>
+        </CardDeck>
+      
+    );
+}   
+
+export default Features
--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Features from './Features/Features'
 //Import components that you create here
 
 function LandingPage(){
     return(
         //Use components that you import here
-        <></>
+        
+        <Features/>
     )
 }
 
