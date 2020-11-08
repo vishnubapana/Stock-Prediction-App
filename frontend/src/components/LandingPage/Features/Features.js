cc63195557afdd6e5f3ebdbc30fbc1127b8523d8
kNgo8249 <kenguin8249@gmail.com>
2020-11-08 17:01:21 -0600

    Updated Features component

--- frontend/src/components/LandingPage/Features/Features.js
+++ frontend/src/components/LandingPage/Features/Features.js
@@ -6,29 +6,32 @@ import {
   
 function Features() {
     return (
-        
-        <CardDeck>
-            <Card>
-                <CardBody>
-                    <CardTitle tag="h4">Guess Prices</CardTitle>
-                    <CardText tag="h6">Get predictions on future stock prices so you can make more informed buying decisions.</CardText>
-                </CardBody>
-            </Card>
-            <Card>
-                <CardBody>
-                    <CardTitle tag="h4">Get Stock Information</CardTitle>
-                    <CardText tag="h6">Get the latest information on stocks, including prices and changes.</CardText>
-                </CardBody>
-            </Card>
-            <Card>
-                <CardBody>
-                    <CardTitle tag="h4">Get Current News</CardTitle>
-                    <CardText tag="h6">Get the current news on things like the weather and company updates.</CardText>
-                </CardBody>
-            </Card>
-        </CardDeck>
-      
+        <div>
+            <h1>Features of this Application</h1>
+            
+            <CardDeck>
+                <Card>
+                    <CardBody>
+                        <CardTitle tag="h4">Guess Prices</CardTitle>
+                        <CardText tag="h6">Get predictions on future stock prices so you can make more informed buying decisions.</CardText>
+                    </CardBody>
+                </Card>
+                <Card>
+                    <CardBody>
+                        <CardTitle tag="h4">Get Stock Information</CardTitle>
+                        <CardText tag="h6">Get the latest information on stocks, including prices and changes.</CardText>
+                    </CardBody>
+                </Card>
+                <Card>
+                    <CardBody>
+                        <CardTitle tag="h4">Get Current News</CardTitle>
+                        <CardText tag="h6">Get the current news on things like the weather and company updates.</CardText>
+                    </CardBody>
+                </Card>
+            </CardDeck>
+            
+        </div>
     );
-}   
-
+    
+}
 export default Features
