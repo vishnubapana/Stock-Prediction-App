bade50b5efd9b45f0d1431f6c82f584da1d8c773
ryannd <ryandimaranan23@gmail.com>
2020-11-08 21:48:33 -0600

    add test for tools title

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import Tools from './components/Tools.js';
+import { shallow } from 'enzyme';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('should show Tools title', () => { 
+    const wrapper = shallow(<Tools />); 
+    expect(wrapper.text().includes('Tools')).toBe(true);
 });
