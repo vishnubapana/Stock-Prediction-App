97fcf0334111f1a9811c33c03f60d02f66b8973b
kNgo8249 <kenguin8249@gmail.com>
2020-11-11 01:20:54 -0600

    Updated some tests

--- frontend/src/components/LandingPage/Features/Features.test.js
+++ frontend/src/components/LandingPage/Features/Features.test.js
@@ -3,28 +3,24 @@ import { screen, render } from '@testing-library/react';
 import Features from './Features'
 import React from 'react';
 
-
-
 describe('Test features component', () => {
-    
-    it('Checks if Features component renders', () => {
-        const wrapper = shallow(<Features/>);
+    const wrapper = shallow(<Features/>);
+
+    it('Checks if Features component renders', () => {   
         expect(wrapper.exists()).toBe(true);
     });
 
     it('Check if title text renders properly', () => {
-        render(<Features/>);
-        const textElement = screen.getByText(/Features of this Application/i);
-        expect(textElement).toBeInTheDocument();
+        expect(wrapper.text().includes("Features of this Application")).toBe(true);
     }); 
 
     it('Check if cards render text properly', () => {
-        render(<Features/>);
-        let cardTitles = [/Guess Prices/i, /Get Stock Information/i, /Get Current News/i]
-        cardTitles.forEach(element => {
-            const textElement = screen.getByText(element);
-            expect(textElement).toBeInTheDocument();
-        });
+         render(<Features/>);
+         let cardTitles = [/Guess Prices/i, /Get Stock Information/i, /Get Current News/i]
+         cardTitles.forEach(element => {
+             const textElement = screen.getByText(element);
+             expect(textElement).toBeInTheDocument();
+         });
     }); 
 
 });
