8f032314a8b9e4f6e3e61b0870a0148a99bd993d
kNgo8249 <kenguin8249@gmail.com>
2020-11-11 01:12:48 -0600

    Added more tests for features component

--- frontend/src/components/LandingPage/Features/Features.test.js
+++ frontend/src/components/LandingPage/Features/Features.test.js
@@ -1,8 +1,30 @@
 import { shallow } from 'enzyme';
+import { screen, render } from '@testing-library/react';
 import Features from './Features'
 import React from 'react';
 
-it('Checks if Features component renders correctly', () => {
-    const wrapper = shallow(<Features/>);
-    expect(wrapper.text().includes("Features of this Application")).toBe(true);
+
+
+describe('Test features component', () => {
+    
+    it('Checks if Features component renders', () => {
+        const wrapper = shallow(<Features/>);
+        expect(wrapper.exists()).toBe(true);
+    });
+
+    it('Check if title text renders properly', () => {
+        render(<Features/>);
+        const textElement = screen.getByText(/Features of this Application/i);
+        expect(textElement).toBeInTheDocument();
+    }); 
+
+    it('Check if cards render text properly', () => {
+        render(<Features/>);
+        let cardTitles = [/Guess Prices/i, /Get Stock Information/i, /Get Current News/i]
+        cardTitles.forEach(element => {
+            const textElement = screen.getByText(element);
+            expect(textElement).toBeInTheDocument();
+        });
+    }); 
+
 });
