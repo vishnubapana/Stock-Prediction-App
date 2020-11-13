9f6aa3e682c436725c22f347ae6b09e47a217054
Vishnu Bapanapalli <vishnubapana@gmail.com>
2020-11-13 00:27:15 -0600

    App.js updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import LandingPage from './components/LandingPage/LandingPage'
-import Team from './components/LandingPage/Team'
 function App() {
   return (
-    <div className="App">
       <LandingPage/>
-      <Team/>
-    </div>
   );
 }
 
