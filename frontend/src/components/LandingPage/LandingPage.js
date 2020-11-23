d5ca76eb4cfa17b0cfcb97a804d374ba185a6238
Vishnu Bapanapalli <vishnubapana@gmail.com>
2020-11-22 22:41:07 -0600

    added Team component to LandingPage.js

--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Team from './Team/Team'
 //Import components that you create here
 
 function LandingPage(){
     return(
         //Use components that you import here
-        <></>
+        <Team/>
     )
 }
 
